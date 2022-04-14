31ae5d17de0ea65d25f814640bca4a96edcfdee1
8XA <69772360+8XA@users.noreply.github.com>
2022-04-14 16:41:53 -0700

    build: location_app's model as UnidadesModel

--- container_home/metrobus_project/location_app/models.py
+++ container_home/metrobus_project/location_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class UnidadesModel(models.Model):
+    """
+    Basic model with the relevant fields for the query endpoints.
+    The label is a unique number for each metrobus unit, then it can work as an primary key ID unit.
+    """
+
+    #Works as an ID
+    label = models.IntegerField(unique=True, primary_key=True)
+    
+    #Position
+    latitude = models.IntegerField()
+    longitude = models.IntegerField()
+
+    #Locality name
+    alcaldia = models.TextField()
+
+    class Meta:
+        #Table name
+        db_table = "Unidades"
+
+        #Order by
+        ordering = ['label']
+
+        #Names for the admin panel
+        verbose_name = "Unidad"
+        verbose_name_plural = "Unidades"
+
+    def __str__(self):
+        return 'Unidad {} en {}'.format(self.label, self.alcaldia)
