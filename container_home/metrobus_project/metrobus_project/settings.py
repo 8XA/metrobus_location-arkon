fc6f4ee0762d80c5df3e88f4a6a2447471f92870
8XA <69772360+8XA@users.noreply.github.com>
2022-04-15 13:35:39 -0700

    built: SECRET_KEY of django as an evironment variable in docker-compose

--- container_home/metrobus_project/metrobus_project/settings.py
+++ container_home/metrobus_project/metrobus_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)_7up+&h@^e)q@pj0-u0p@!f!*(4sd&^4e%$-uhjau4#jfmmzg'
+SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
