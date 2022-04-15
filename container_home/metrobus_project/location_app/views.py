620fc25dc3f0d3d2ee52a9a01ab667aa0f41a243
8XA <69772360+8XA@users.noreply.github.com>
2022-04-14 21:51:32 -0700

    built: Microbuses information getter module and a first aproximation of the FetchData view

--- container_home/metrobus_project/location_app/views.py
+++ container_home/metrobus_project/location_app/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 
 from location_app.models import UnidadesModel
 from location_app.serializers import UnidadesSerializer
+from location_app.microbuses_information import get_units_information
 
 
 class AvailableUnits(APIView):
@@ -23,5 +24,17 @@ class UnitsInLocality(APIView):
 
 
 class FetchData(APIView):
-    pass
+    
+    def get(self, request):
+
+        data = get_units_information()
+        serializer = UnidadesSerializer(data=data, many=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def __update(self, request):
+        pass
 
