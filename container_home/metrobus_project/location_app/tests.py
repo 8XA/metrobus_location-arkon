9b77f75c4b9a074090be59657c71047b410fce69
8XA <69772360+8XA@users.noreply.github.com>
2022-04-17 19:05:23 -0700

    test: UnitLocation. It works with the database objects.

--- container_home/metrobus_project/location_app/tests.py
+++ container_home/metrobus_project/location_app/tests.py
@@ -1,10 +1,11 @@
 #from django.test import TestCase
 from unittest import TestCase
+from location_app.models import UnidadesModel
 from location_app.views import AvailableUnits
+from location_app.views import UnitLocation
 
 class LocationAppTest(TestCase):
     #Test for the AvailableUnits View
-    #It should get a JSON with a list units
     def test_available_units(self):
         view_instance = AvailableUnits()
         get_method = view_instance.get(None)
@@ -20,7 +21,23 @@ class LocationAppTest(TestCase):
         assert get_method.status_text == 'OK'
 
     def test_unit_location(self):
-        pass
+        view_instance = UnitLocation()
+        LABEL = 112
+        FIELDS_NUM = 4
+        get_method = view_instance.get(None, LABEL)
+        if 2 > len(UnidadesModel.objects.filter(label=LABEL)) > 0:
+            #It has only the four valid fields
+            assert len(get_method.data) == FIELDS_NUM
+            assert len([field in ['label', 'longitude', 'latitude', 'alcaldia'] for field in get_method.data]) == FIELDS_NUM
+            #Verifies if the data type is right
+            assert isinstance(get_method.data['label'], int)
+            assert isinstance(get_method.data['longitude'], str)
+            assert isinstance(get_method.data['latitude'], str)
+            assert isinstance(get_method.data['alcaldia'], str)
+            #HTTP Response is OK
+            assert get_method.status_text == 'OK'
+        else:
+            assert get_method.status_text == 'Not Found'
     
     def test_available_localities(self):
         pass
