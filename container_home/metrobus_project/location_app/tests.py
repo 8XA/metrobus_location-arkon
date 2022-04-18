2c001d0419785ba71012f449a8d855a44a80c771
8XA <ochoa.ssa@gmail.com>
2022-04-17 18:30:15 -0700

    test: AvailableUnits. It works with the database objects.

--- container_home/metrobus_project/location_app/tests.py
+++ container_home/metrobus_project/location_app/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+#from django.test import TestCase
+from unittest import TestCase
+from location_app.views import AvailableUnits
 
-# Create your tests here.
+class LocationAppTest(TestCase):
+    #Test for the AvailableUnits View
+    #It should get a JSON with a list units
+    def test_available_units(self):
+        view_instance = AvailableUnits()
+        get_method = view_instance.get(None)
+        units_list = get_method.data['available_units']
+        units_validation = [isinstance(unit, int) for unit in units_list]  
+        non_repeated_units = list(set(units_list))
+
+        #All the labels/ID units are integers
+        assert all(units_validation)
+        #Every unit ID is unique
+        assert len(units_list) == len(non_repeated_units)
+        #MEthod return 200 OK status
+        assert get_method.status_text == 'OK'
+
+    def test_unit_location(self):
+        pass
+    
+    def test_available_localities(self):
+        pass
+    
+    def test_units_in_locality(self):
+        pass
+
